1575bd61101bf7696cee0adbffe09e3cf7997b54
Marcos G M <gmarcos38@gmail.com>
2024-02-11 18:03:53 -0300

    creating a type

---
+++ 3/main.go
@@ -0,0 +1,17 @@
+package main
+
+const a = "Hello World"
+
+type ID int // Declare of a type with receives a primal type (this point of the class FOR ME don't make sense
+
+var (
+	b bool    = true
+	c int     = 10
+	d string  = "Wesley"
+	e float64 = 1.2
+	f ID      = 1
+)
+
+func main() {
+	println(f)
+}
